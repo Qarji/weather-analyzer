2c39b4f4c080c429020788533fa6ebb5d1b5c9bf
Qorsi <86189310+Qarji@users.noreply.github.com>
2024-10-08 12:37:19 +0400

    Update Weather Analyzer.py

--- Weather Analyzer.py
+++ Weather Analyzer.py
@@ -4,8 +4,9 @@ from meteostat import Point, Daily
 import numpy as np
 import pandas as pd
 
-start, end = datetime(2021, 1, 1), datetime(2022, 12, 31)
-point_x, point_y, point_z = 48.9301, -93.4077, 70
+# пример:
+# start, end = datetime(2021, 1, 1), datetime(2022, 12, 31)
+# point_x, point_y, point_z = 48.9301, -93.4077, 70
 
 def select_data(start, end, point_x, point_y, point_z):
     geo_dot = Point(point_x, point_y, point_z) # Создаём точку
@@ -13,7 +14,6 @@ def select_data(start, end, point_x, point_y, point_z):
     data = Daily(geo_dot, start, end)
     data = data.fetch()
     return data
-weath_data = select_data(start, end, point_x, point_y, point_z)
 
 def weather_index_search():
     # Индексы настроены на умеренную широту
@@ -30,7 +30,6 @@ def weather_index_search():
     print(f'Ratio of extreme to normal: {attitude_ext_norm_idx} | {len(prcp_to_day_ext)}/{len(prcp_to_day_norm)}')
     print(f'-------------------------\nTemperature trend: {round(trend[-1]-trend[0], 2)}')
 
-
 def linear_trend():
     # Линейная аппроксимация с учетом пропусков
     if weath_data['tavg'].isnull().sum() > 0:
@@ -40,9 +39,6 @@ def linear_trend():
     coefficients = np.polyfit(x, weath_data['tavg'], 1)
     trend = np.polyval(coefficients, x)
     return trend
-trend = linear_trend()
-weather_index_search()
-
 
 def graphics_drawing():
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
@@ -64,4 +60,3 @@ def graphics_drawing():
     ax2.grid(True)
     plt.tight_layout()
     plt.show()
-graphics_drawing()
